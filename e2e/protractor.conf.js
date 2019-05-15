81f1daa5b9e81904c90a79a20fdbabc6421a7a9d
Your Name <youremail>
2019-05-15 22:35:09 +0300

    Added unit for Travis CI using Headless Chrome

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -5,11 +5,14 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
+  specs: ['./src/**/*.e2e-spec.ts'],
   capabilities: {
-    'browserName': 'chrome'
+    // chromeOptions is needed for travis.ci
+    chromeOptions: {
+      args: ['--headless']
+    },
+    //
+    browserName: 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -23,6 +26,8 @@ exports.config = {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine
+      .getEnv()
+      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
