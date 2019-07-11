d398e2ce31919abf1a4096244f54e27aeea0a2ac
dmitriy-ka <youremail>
2019-07-12 01:29:33 +0300

    Crop image - basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,16 @@ export class AppComponent {
   title = 'ng-app';
   name = 1;
 
+  imageSrc;
+
   fileChangedEvent: any;
   constructor() {}
 
   onChangeFile(event) {
     this.fileChangedEvent = event;
+  }
+
+  imageCropped(event) {
+    this.imageSrc = event;
   }
 }
--- src/app/ngx-cropper/components/ngx-cropper/ngx-cropper.component.ts
+++ src/app/ngx-cropper/components/ngx-cropper/ngx-cropper.component.ts
@@ -10,10 +10,13 @@ import {
   ElementRef,
   HostListener,
   OnDestroy,
-  Renderer2
+  Renderer2,
+  NgZone,
+  Output,
+  EventEmitter
 } from '@angular/core';
 import { Subscription, fromEvent, Subject } from 'rxjs';
-import { debounceTime, takeUntil, map, tap } from 'rxjs/operators';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'ngx-cropper',
@@ -22,28 +25,36 @@ import { debounceTime, takeUntil, map, tap } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NgxCropperComponent implements OnInit, OnChanges, OnDestroy {
-  private originalImage: any;
-  private originalBase64: string;
-  private ratio: number;
-  private moveSubscription: Subscription;
-  private stopMove$: Subject<boolean> = new Subject();
-  private onDestroy$: Subject<any> = new Subject();
-
   @ViewChild('cropArea') cropArea: ElementRef;
   @ViewChild('cropImage') cropImage: ElementRef;
   @ViewChild('cropper') cropper: ElementRef;
+  @ViewChild('cropperDragElement') cropperDragElement: ElementRef;
 
   @Input() format: 'png' | 'jpeg' = 'png';
+  @Input() downloadFileName = 'image';
+  @Input()
+  set quality(value) {
+    value = isNaN(Number(value)) ? 0.1 : value;
+    if (value < 0) {
+      this._quality = 0;
+    } else if (value > 1) {
+      this._quality = 1;
+    } else {
+      this._quality = Number(value);
+    }
+  }
+  get quality() {
+    return this._quality;
+  }
   @Input()
   set aspectRatio(value) {
     if (value < 10 || value > 10) {
       value = 1;
     }
-    this.ratio = value;
+    this._ratio = value;
   }
-
   get aspectRatio() {
-    return this.ratio;
+    return this._ratio;
   }
 
   @Input()
@@ -55,95 +66,189 @@ export class NgxCropperComponent implements OnInit, OnChanges, OnDestroy {
         // TODO:
         return;
       }
-      this.initialize(file);
+      this.initCropper(file);
     }
   }
 
-  imageSrc;
+  @Output() imageCropped = new EventEmitter();
+
+  private originalImage: HTMLImageElement;
+  private originalSize: Dimensions = {
+    width: 0,
+    height: 0
+  };
+  private originalBase64: any;
+  private canvas: HTMLCanvasElement;
+  private _ratio: number;
+  private _quality: number;
+
+  private dragging$: Subscription;
+  private stopMove$: Subject<boolean> = new Subject();
+  private onDestroy$: Subject<any> = new Subject();
+
   showLoader = false;
 
-  @HostListener('document:mouseup') clearListeners() {
+  @HostListener('document:mouseup') clearListeners(event) {
     this.stopMove$.next(true);
   }
 
-  constructor(private cdr: ChangeDetectorRef, private renderer: Renderer2) {}
-
-  ngOnInit() {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log({ changes });
+  get imageSrc(): string {
+    if (this.originalImage) {
+      return this.originalImage.src;
+    }
   }
 
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private renderer: Renderer2,
+    private ngZone: NgZone
+  ) {}
+
+  ngOnInit() {
+    this.stopMove$.pipe(takeUntil(this.onDestroy$)).subscribe(res => {
+      if (this.dragging$) {
+        this.crop();
+      }
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {}
+
   ngOnDestroy() {
-    this.moveSubscription.unsubscribe();
+    this.dragging$.unsubscribe();
     this.stopMove$.next();
     this.stopMove$.complete();
     this.onDestroy$.next();
     this.onDestroy$.complete();
   }
 
-  get image(): boolean {
-    return this.originalImage;
-  }
-
-  private isFileValid(file) {
-    return true;
-  }
-
-  private initialize(file: any) {
-    this.showLoader = true;
-    this.originalImage = file;
-
-    const reader = new FileReader();
-    reader.onload = (event: any) => {
-      this.imageSrc = event.target.result;
-      this.showLoader = false;
-      this.cdr.markForCheck();
-    };
-    reader.readAsDataURL(file);
-  }
-
-  showCropArea() {}
-
-  startMove(clickEvent, type: MoveType = 'move') {
-    console.log('START MOVE', clickEvent);
-    this.moveSubscription = fromEvent(document, 'mousemove')
+  dragCropper(clickEvent, type: MoveType = 'move') {
+    if (clickEvent.srcElement !== this.cropperDragElement.nativeElement) {
+      return;
+    }
+    this.dragging$ = fromEvent(document, 'mousemove')
       .pipe(takeUntil(this.stopMove$))
       .subscribe((event: MouseEvent) => {
         this.moveCropper(clickEvent, event);
       });
   }
 
+  crop() {
+    this.ngZone.runOutsideAngular(() => {
+      const {
+        offsetLeft,
+        offsetTop,
+        offsetWidth,
+        offsetHeight
+      } = this.cropper.nativeElement;
+
+      // Ratio between original image size and cropArea size
+      const ratio =
+        this.originalSize.width / this.cropArea.nativeElement.clientWidth;
+
+      const canvas = document.createElement('canvas');
+      canvas.width = offsetWidth * ratio;
+      canvas.height = offsetHeight * ratio;
+
+      canvas
+        .getContext('2d')
+        .drawImage(
+          this.originalImage,
+          offsetLeft * ratio,
+          offsetTop * ratio,
+          offsetWidth * ratio,
+          offsetHeight * ratio,
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        );
+
+      this.ngZone.run(() => {
+        this.canvas = canvas;
+        this.imageCropped.emit(canvas.toDataURL('image/png', this.quality));
+      });
+    });
+  }
+
+  downloadImage() {
+    const link = document.createElement('a');
+    link.href = this.canvas.toDataURL();
+    link.download = `${this.downloadFileName}.${this.format}`;
+    link.click();
+  }
+
+  noop(event?: MouseEvent) {
+    return false;
+  }
+
+  private isFileValid(file) {
+    return true;
+  }
+
+  private initCropper(file: any) {
+    this.showLoader = true;
+
+    const reader = new FileReader();
+    reader.onload = (event: ProgressEvent) => {
+      this.showLoader = false;
+      this.cdr.markForCheck();
+      this.onLoadImage(reader.result as string);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  private onLoadImage(imageBase64: string) {
+    this.originalBase64 = imageBase64;
+
+    this.originalImage = new Image();
+    this.originalImage.onload = () => {
+      this.originalSize.width = this.originalImage.width;
+      this.originalSize.height = this.originalImage.height;
+      this.crop();
+      this.cdr.markForCheck();
+    };
+    this.originalImage.src = imageBase64;
+  }
+
   private moveCropper(clickEvent: MouseEvent, moveEvent: MouseEvent) {
-    const clickOffsetX = clickEvent.offsetX;
-    const clickoffsetY = clickEvent.offsetY;
+    this.ngZone.runOutsideAngular(() => {
+      const clickOffsetX = clickEvent.offsetX;
+      const clickoffsetY = clickEvent.offsetY;
 
-    const areaRect = this.getCroppAreaClientRect();
-    const cropperRect = this.getCropperClientRect();
+      const areaRect = (this.cropArea
+        .nativeElement as HTMLElement).getBoundingClientRect();
+      const cropperRect = (this.cropper
+        .nativeElement as HTMLElement).getBoundingClientRect();
 
-    const eventPageX = moveEvent.pageX;
-    const eventPageY = moveEvent.pageY;
-    const eventLeftMin = areaRect.left + clickOffsetX;
-    const eventLeftMax = areaRect.right - clickOffsetX;
-    const eventTopMin = areaRect.top + clickoffsetY;
-    const eventTopMax = areaRect.bottom - clickoffsetY;
+      const eventPageX = moveEvent.pageX;
+      const eventPageY = moveEvent.pageY;
+      const eventLeftMin = areaRect.left + clickOffsetX;
+      const eventLeftMax = areaRect.right - clickOffsetX;
+      const eventTopMin = areaRect.top + clickoffsetY;
+      const eventTopMax = areaRect.bottom - clickoffsetY;
 
-    let left, top;
-    if (eventPageX <= eventLeftMin) {
-      left = 0;
-    } else if (eventPageX >= eventLeftMax) {
-      left = areaRect.width - cropperRect.width;
-    } else {
-      left = eventPageX - areaRect.left - clickOffsetX;
-    }
-    if (eventPageY <= eventTopMin) {
-      top = 0;
-    } else if (eventPageY >= eventTopMax) {
-      top = areaRect.height - cropperRect.height;
-    } else {
-      top = eventPageY - areaRect.top - clickoffsetY;
-    }
-    this.setCropperPosition(left, top);
+      // TODO: calc with scroll height
+
+      let left, top;
+      if (eventPageX <= eventLeftMin) {
+        left = 0;
+      } else if (eventPageX >= eventLeftMax) {
+        left = areaRect.width - cropperRect.width;
+      } else {
+        left = eventPageX - areaRect.left - clickOffsetX;
+      }
+      if (eventPageY <= eventTopMin) {
+        top = 0;
+      } else if (eventPageY >= eventTopMax) {
+        top = areaRect.height - cropperRect.height;
+      } else {
+        top = eventPageY - areaRect.top - clickoffsetY;
+      }
+      this.ngZone.run(() => {
+        this.setCropperPosition(left, top);
+      });
+    });
   }
 
   private setCropperPosition(left: number, top: number): void {
@@ -156,22 +261,11 @@ export class NgxCropperComponent implements OnInit, OnChanges, OnDestroy {
     this.renderer.setStyle(this.cropper.nativeElement, 'left', `${left}px`);
     this.renderer.setStyle(this.cropper.nativeElement, 'top', `${top}px`);
   }
-
-  private getCroppAreaClientRect(): ClientRect {
-    return (this.cropArea.nativeElement as HTMLElement).getBoundingClientRect();
-  }
-
-  private getCropperClientRect(): ClientRect {
-    return (this.cropper.nativeElement as HTMLElement).getBoundingClientRect();
-  }
-  noop(event?) {
-    return false;
-  }
-
-  private clear() {
-    this.imageSrc = null;
-    this.originalImage = null;
-  }
 }
 
 export type MoveType = 'move' | 'resize';
+
+export interface Dimensions {
+  width: number;
+  height: number;
+}
